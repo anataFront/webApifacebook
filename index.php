0d502fb11b1d920b02f7f1824d5a884a77a032f1
josue <josue.ladines.pe@gmail.com>
2017-11-26 11:49:04 -0500

    añadi imagen y corregi descripcion

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8"/>
     <title><?php echo $about->name ?></title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
-    <link href="https://file.myfontastic.com/XEYMpRfP5Gdp65e8cdDeW6/icons.css" rel="stylesheet"/>
+    <link href="" rel="stylesheet"/>
     <link rel="stylesheet" href="css/style.css"/>
   </head>
   <body>
@@ -43,9 +43,8 @@
     <header class="slider scrollspy" id="inicio">
       <ul class="slides">
         <?php foreach ($port as $portas): ?>
-        <li><img src="<?php echo $portas->images[0]->source; ?>"/>
+        <li><img src="img/ataydesatafrente.jpg"/>
           <div class="caption center-align">
-            <h2 class="main-header__title">ATANDO Y DESATANDO EMOCIONES</h2>
           </div>
         </li>
         <?php endforeach ?>
@@ -54,32 +53,19 @@
     <main class="main scrollspy"  id="nosotros">
       <div class="container">
         <div class="row main-container">
+
           <div class="col s12 l6">
             <div class="video-container">
               <iframe width="560" height="315" src="https://www.youtube.com/embed/-gr40bUCkEc" frameborder="0" allowfullscreen></iframe></div>
             </div>
           <div class="col s12 l6">
-            <h3 class="main-title main--title">ATANDO Y DESATANDO EMOCIONES</h3>
-            <h5 class="light grey-text text-lighten-3 main-header__description"> Ata y Desata es una empresa especializada en organización de eventos, convenciones, producción fotográfica, activaciones y comunicación interna. Siempre caracterizados con el profesionalismo, calidad e inspiración que nuestros clientes prefieren.
-                                                                               Ofrecemos el concepto, producción y organización global de su evento. Nuestra experiencia y creatividad, junto con la pasión y dedicación de nuestro equipo logran que cualquier evento sea posible.
-                                                                                Somos especialistas en conseguir que el evento que quiere realizar sea original, divertido y efectivo. Nuestra larga experiencia nos da la confianza suficiente para garantizarle un éxito total.</h5>
-            <p class="main-description"><?php echo $descripcion[0]; ?></p>
+                        <p class="main-header__description"> Ata y Desata es una empresa especializada en organización de eventos, convenciones, producción fotográfica, activaciones y comunicación interna. Siempre caracterizados con el profesionalismo, calidad e inspiración que nuestros clientes prefieren.
+                                                  Ofrecemos el concepto, producción y organización global de su evento. Nuestra experiencia y creatividad, junto con la pasión y dedicación de nuestro equipo logran que cualquier evento sea posible.
+                                                    Somos especialistas en conseguir que el evento que quiere realizar sea original, divertido y efectivo. Nuestra larga experiencia nos da la confianza suficiente para garantizarle un éxito total. </p>
           </div>
         </div>
       </div>
-      <div class="main-container__services">
-        <div class="container">
-          <div class="row">
-            <div class="col s12 main-container__services--description">
-              <p><?php echo $descripcion[1]; ?></p>
-            </div>
-          </div>
-        </div>
-        <div class="row main-container__services--content">
-
-        </div>
-      </div>
-    </main>
+        </main>
     <section class="main-publisher scrollspy" id="publicaciones">
       <div class="container">
         <div class="row">
@@ -132,11 +118,11 @@
         </div>
       </div>
     </section>
-    <section class="main-events scrollspy" id="eventos">
+    <section class="main-events scrollspy" id="nuestrosservicios">
       <div class="container">
         <div class="row">
           <div class="col s12">
-            <h3 class="main-events__title main--title">NUESTRO SERVICIOS</h3>
+            <h3 class="main-events__title main--title">NUESTROS SERVICIOS</h3>
           </div>
         </div>
       </div>
